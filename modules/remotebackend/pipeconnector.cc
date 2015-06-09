994fc14031c984afc383634a610bcdaa1568c1b8
Christian Hofstaedtler <zeha@debian.org>
2015-06-09 21:33:52 +0200

    Imported Upstream version 3.4.5

--- modules/remotebackend/pipeconnector.cc
+++ modules/remotebackend/pipeconnector.cc
@@ -17,7 +17,8 @@ PipeConnector::PipeConnector(std::map<std::string,std::string> options) {
 
   d_pid = -1;
   d_fp = NULL;
-  launch();
+  d_fd1[0] = d_fd1[1] = -1;
+  d_fd2[0] = d_fd2[1] = -1;
 }
 
 PipeConnector::~PipeConnector(){
